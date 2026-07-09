9c1f2164c8e6baac31fe7e0ac5696feaa10f22fd
ruqayaalsammak <ruqayaalsammak2017@gmail.com>
2026-07-09 10:03:05 +0300

    c

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.get("/", async (req, res) => {
+  res.send("hello, friend!");
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
